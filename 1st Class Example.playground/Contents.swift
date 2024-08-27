9513edc066b95ce0684112d537a4bb11b5c48b19
Vinicius Rodrigues <S543619@NWMISSOURI.EDU>
2024-08-27 10:35:33 -0500

    1st Class Example - Version 2
    Added an if/else statement, while loop and for loop

--- 1st Class Example.playground/Contents.swift
+++ 1st Class Example.playground/Contents.swift
@@ -13,4 +13,22 @@ let k = 20
 print(k)
 print(k-2)
 
+while (n < 15) {
+    n += 1
+    print(n)
+}
 
+var num = 12
+
+if (num != 12) {
+    print("Number is not 12")
+}
+else {
+    print("Number is indeed 12!")
+}
+
+let rackets = ["Wilson Ultra", "Yonex Ezone", "Head Prestige"]
+
+for racket in rackets {
+    print("\(racket) is on Tennis Warehouse!")
+}
