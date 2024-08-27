c8950f0a609ddfb81e27e2c6b7a636cb623bd22a
Vinicius Rodrigues <S543619@NWMISSOURI.EDU>
2024-08-27 10:22:39 -0500

    1st Class Example
    Just messed around to get to know how to use Xcode (Swift)

---
+++ 1st Class Example.playground/Contents.swift
@@ -0,0 +1,16 @@
+import UIKit
+
+var greeting = "Hello, playground, from Vini!"
+print(greeting)
+
+var n = 10
+var m = 20
+var sum = n + m
+print(sum)
+print(n+2)
+
+let k = 20
+print(k)
+print(k-2)
+
+
